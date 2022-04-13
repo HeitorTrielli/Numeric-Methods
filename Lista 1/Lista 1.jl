56eca26f1419f2a0fdce19c0ab931634e272aca9
HeitorTrielli <heitortrielli@gmail.com>
2022-04-13 06:58:56 -0300

    commit

--- Lista 1/Lista 1.jl
+++ Lista 1/Lista 1.jl
@@ -2,33 +2,34 @@ using Pkg, Distributions, Random
 
 # Método de Tauchen:
 
- # função que vai criar o grid, dados os parâmetros
-gridify = function (n, sigma = 0.007, rho = 0.95, m = 3)
-    theta_max = m*sigma/(sqrt(1-rho^2)) # definindo o maior valor do grid
-    theta_min = -theta_max # definindo o menor valor do grid
+    # função que vai criar o grid, dados os parâmetros
+    gridify = function (n, sigma = 0.007, rho = 0.95, m = 3)
+        theta_max = m*sigma/(sqrt(1-rho^2)) # definindo o maior valor do grid
+        theta_min = -theta_max # definindo o menor valor do grid
 
-    return LinRange(theta_min, theta_max, n) # Cria um vetor de n pontos entre theta_max e theta_min em que a distancia entre os pontos sequencias é igual
-end
-
-# função que vai computar as probabilidades de transição dado o grid e os parâmetros
-tauchen = function (grid, sigma = 0.007, rho = 0.95, m = 3) 
-    d = Normal() # d vira normal(0,1), que será usado para computar a PDF dos erros na hora de achar as probabilidades de transição
-    delta = (maximum(grid)-minimum(grid))/(length(grid)-1) # distância dos pontos subsequentes do grid
-
-    vec_1 = cdf(d,((minimum(grid) .- rho*grid .+ delta/2)/sigma)) # vetor das probabilidades de ir para o menor valor do grid, dado cada estado anterior do grid; cdf(d, x) retorna a cdf da distribuição d no valor x
-    vec_N = 1 .- cdf(d,((maximum(grid) .- rho*grid .- delta/2)/sigma)) # análogo para o maior valor do grid
-    grid_interno = grid[2:(length(grid)-1)] # valores não extremos do grid
-
-    pij = function(j, i = grid) # função que vai computar o vetor de probabilidades de ir para o estado (não extremo) j dado cada estado anterior do grid
-        cdf(d,((j .+ delta/2 .- rho*i)/sigma)) - cdf(d,((j .- delta/2 .- rho*i)/sigma))                             
+        return LinRange(theta_min, theta_max, n) # Cria um vetor de n pontos entre theta_max e theta_min em que a distancia entre os pontos sequencias é igual
     end
 
-    mat_interna = reduce(hcat, map(pij, grid_interno)) # map: aplica pij em cada ponto do grid interno; reduce: transforma o vetor de vetores que vem do map em uma matriz
+    # função que vai computar as probabilidades de transição dado o grid e os parâmetros
+    tauchen = function (grid, sigma = 0.007, rho = 0.95, m = 3) 
+        d = Normal() # d vira normal(0,1), que será usado para computar a PDF dos erros na hora de achar as probabilidades de transição
+        delta = (maximum(grid)-minimum(grid))/(length(grid)-1) # distância dos pontos subsequentes do grid
 
-    CP = [vec_1 mat_interna vec_N] # combinando os vetores de transição para ter a matriz de transição
+        vec_1 = cdf(d,((minimum(grid) .- rho*grid .+ delta/2)/sigma)) # vetor das probabilidades de ir para o menor valor do grid, dado cada estado anterior do grid; cdf(d, x) retorna a cdf da distribuição d no valor x
+        vec_N = 1 .- cdf(d,((maximum(grid) .- rho*grid .- delta/2)/sigma)) # análogo para o maior valor do grid
+        grid_interno = grid[2:(length(grid)-1)] # valores não extremos do grid
 
-    return CP
-        
-end
+        pij = function(j, i = grid) # função que vai computar o vetor de probabilidades de ir para o estado (não extremo) j dado cada estado anterior do grid
+            cdf(d,((j .+ delta/2 .- rho*i)/sigma)) - cdf(d,((j .- delta/2 .- rho*i)/sigma))                             
+        end
 
-probs_tauchen = tauchen(gridify(9))
+        mat_interna = reduce(hcat, map(pij, grid_interno)) # map: aplica pij em cada ponto do grid interno; reduce: transforma o vetor de vetores que vem do map em uma matriz
+
+        CP = [vec_1 mat_interna vec_N] # combinando os vetores de transição para ter a matriz de transição
+
+        return CP
+            
+    end
+
+    probs_tauchen = tauchen(gridify(9))
+    round_tauchen = map(x -> round(x, digits =3), probs_tauchen)
--- Lista 1/Lista 1.r
+++ Lista 1/Lista 1.r
@@ -1,36 +1,36 @@
 # Método de Tauchen
 
-# função que vai fazer o grid de n pontos, dados os parâmetros
-gridify <- function(n, sigma = 0.007, rho = 0.95, m = 3){
-    theta_max <- m*sigma/sqrt(1-rho^2)
-    theta_min <- -theta_max
+    # função que vai fazer o grid de n pontos, dados os parâmetros
+    gridify <- function(n, sigma = 0.007, rho = 0.95, m = 3){
+        theta_max <- m*sigma/sqrt(1-rho^2)
+        theta_min <- -theta_max
 
-    grid <- seq(theta_min, theta_max, length.out = n) # dividindo [theta_min, theta_max] em n pontos
-    return(grid)
+        grid <- seq(theta_min, theta_max, length.out = n) # dividindo [theta_min, theta_max] em n pontos
+        return(grid)
 
-}
-
-# função que vai trazer a matriz de transição 
-tauchen <- function(grid, sigma = 0.007, rho = 0.95, m = 3){
-    
-    delta = (max(grid)-min(grid))/(length(grid)-1) # tamanho dos saltos entre os pontos do grid
-
-    
-    vec_1 <- pnorm((min(grid)-rho*grid+delta/2)/sigma) # vetor de transição para o menor valor do grid dado cada estado anterior; pnorm(x) retorna a cdf da normal no valor x
-    vec_N <- 1- pnorm((max(grid)-rho*grid-delta/2)/sigma) # análogo para o maior valor do grid
-    grid_interno <- grid[2:(length(grid)-1)] # valores não extremos do grid
-
-    # função que retorna o vetor de transição para o estado (não extremo) j dado cada estado anterior
-    pij <- function(j, i = grid){ 
-        pnorm((j + delta/2-rho*i)/sigma) - pnorm((j - delta/2-rho*i)/sigma)  
     }
 
-    mat_interna <- sapply(grid_interno, pij) # aplicando pij em cada valor do grid interno para ter a matriz de transição dos valores não extremos
+    # função que vai trazer a matriz de transição 
+    tauchen <- function(grid, sigma = 0.007, rho = 0.95, m = 3){
+        
+        delta = (max(grid)-min(grid))/(length(grid)-1) # tamanho dos saltos entre os pontos do grid
 
-    return(cbind(vec_1, mat_interna, vec_N)) # combinando as transições extremas com as não extremas
-}
+        
+        vec_1 <- pnorm((min(grid)-rho*grid+delta/2)/sigma) # vetor de transição para o menor valor do grid dado cada estado anterior; pnorm(x) retorna a cdf da normal no valor x
+        vec_N <- 1- pnorm((max(grid)-rho*grid-delta/2)/sigma) # análogo para o maior valor do grid
+        grid_interno <- grid[2:(length(grid)-1)] # valores não extremos do grid
 
-probs_tauchen <- tauchen(gridify(9))
+        # função que retorna o vetor de transição para o estado (não extremo) j dado cada estado anterior
+        pij <- function(j, i = grid){ 
+            pnorm((j + delta/2-rho*i)/sigma) - pnorm((j - delta/2-rho*i)/sigma)  
+        }
 
+        mat_interna <- sapply(grid_interno, pij) # aplicando pij em cada valor do grid interno para ter a matriz de transição dos valores não extremos
+
+        return(cbind(vec_1, mat_interna, vec_N)) # combinando as transições extremas com as não extremas
+    }
+
+    probs_tauchen <- tauchen(gridify(9))
+    round_tauchen <- round(probs_tauchen, digits = 3)
 
 
