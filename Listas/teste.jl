a948d510243466e86ffe88b28ef714d36d5e2ff6
HeitorTrielli <heitortrielli@gmail.com>
2022-05-07 19:52:09 -0300

    commit

--- Listas/teste.jl
+++ Listas/teste.jl
@@ -1,3 +1,4 @@
+using BenchmarkTools, Plots
 include("Lista 1.jl"); # Para usar as funções definidas na lista 1
 
 # Definindo as variáveis
@@ -10,62 +11,122 @@ kss = (alpha / (1 / beta - 1 + delta))^(1 / (1 - alpha));
 grid_z = exp.(tauchen(7)[2]); # Valores que exp(z_t) pode tomar 
 prob_z = tauchen(7)[1]; # Matriz de transição de z
 grid_k = LinRange(0.75*kss, 1.25*kss, 500); # Grid de capital
+v0 = zeros(length(grid_k), length(grid_z));
 
 # A função de utilidade
 utility = function(c; mu = 2)
    return (float(c)^(1 - mu) - 1)/(1 - mu)
 end
 
-zmat = repeat(grid_z, 1, k_len)'
 z = grid_z
 p_z = prob_z
 k = grid_k
 tol = 10e-4    
 k_len = length(grid_k)
 z_len = length(grid_z)
-kmat = repeat(grid_k, 1, z_len)
+value, c, k_line, v_next = zeros(k_len, z_len), zeros(k_len, z_len), zeros(k_len, z_len), zeros(k_len, z_len) 
+
+count = 0
 
 
-# Value function, policy function on c, policy function on k and variable for iteration
+# Concavity
+value_function_concave = function(;v_0 = v0, z = grid_z, p_z = prob_z, k = grid_k, tol = 1e-4,
+    beta = 0.987, mu = 2, alpha = 1 / 3, delta = 0.012, rho = 0.95, sigma = 0.007, inert = 0)
+    k_len = length(grid_k)
+    z_len = length(grid_z)
 
-value, c, k_line, v_next = zeros(k_len, z_len), zeros(k_len, z_len), 
-                            zeros(k_len, z_len), zeros(k_len, z_len) 
-count = 101
+    # Value function, policy function on c, policy function on k and variable for iteration
+    c, k_line, v_next = zeros(k_len, z_len), zeros(k_len, z_len), zeros(k_len, z_len)
+    value = v_0
+    count = 0
+    error = 1
+    while error > tol
 
+        Threads.@threads for state in 1:z_len
+            for capital in 1:k_len  
+                k_possible = k[z[state]*(k[capital]^alpha) + (1 - delta)*k[capital] .- k .> 0]    
+                v_possible = value[z[state]*(k[capital]^alpha) + (1 - delta)*k[capital] .- k .> 0, :]               
+                val = utility.(z[state]*(k[capital]^alpha) .- k_possible .+ (1 - delta)*k[capital]) .+ beta*v_possible*p_z[state, :]
+                v_next[capital, state], k_line[capital, state] = lastpos(val, k_possible)
+                c[capital, state] = z[state]*(k[capital]^alpha) - k_line[capital, state] + (1 - delta)* k[capital]
+            end # for k
+        end # for z
+        error = maximum(abs.(value - v_next))
+        value = copy((1 - inert)*v_next + inert*value)
+        count = count + 1
+    end # while
 
-    value = copy(v_next)
-    for state in 1:z_len    
-        for capital in 1:k_len
-            if  count%10 == 0 || count < 100
-                    k_possible = k[z[state]*(k[capital]^alpha) .- k .+ (1 - delta)*k[capital] .> 0]    
-                    v_possible = value[z[state]*(k[capital]^alpha) .- k .+ (1 - delta)*k[capital] .> 0, :]    
-                    val = utility.(z[state]*(k[capital]^alpha) .- k_possible .+ (1 - delta)*k[capital]) .+ beta*v_possible*p_z[state, :]
-                    v_next[capital, state] = maximum(val)
-                    k_line[capital, state] = k_possible[argmax(val)]
-                    c[capital, state] = z[state]*(k[capital]^alpha) - k_line[capital, state] + (1 - delta)* k[capital]
-            else                     
-                v_next[capital, state] = utility(c[capital, state]) + beta*value[capital,:]'*p_z[state, :]
-            end
-
-        end # for k
-    end # for z
-
-v_next == utility.(c)+beta*test*p_z
-
-
-value[15,:]'*p_z[2,:] == (value*p_z')[15,2]
-
-value[1,:]'*p_z[1,:]
-
-(value*p_z')[1]
-
-v_next = utility.(c) + beta*value*p_z
+    return value, k_line, c
+end; # function
 
 
-utility(c[capital, state])
+concave = @time value_function_concave()
 
-v_possible[state, :]
+brute = @time value_function_brute()
 
-value[1,:]'*p_z[1,:]
+state = 5
+capital = 500
 
-30%30
+k_possible = k[z[state]*(k[capital]^alpha) + (1 - delta)*k[capital] .- k .> 0]    
+v_possible = value[z[state]*(k[capital]^alpha) + (1 - delta)*k[capital] .- k .> 0, :]               
+val = utility.(z[state]*(k[capital]^alpha) .- k_possible .+ (1 - delta)*k[capital]) .+ beta*v_possible*p_z[state, :]
+lagval = replace(lag(val), missing => 0.0)
+index = findlast(val - lagval .> 0)
+v_next[capital, state] = val[index]
+k_line[capital, state] = k_possible[index]
+c[capital, state] = z[state]*(k[capital]^alpha) - k_line[capital, state] + (1 - delta)* k[capital]
+
+
+lastpos = function(val, k_possible)
+    v = 0
+    k = 0
+    for i in 2:length(val)
+        if val[i] < val[i-1]
+            v = val[i-1]
+            k = k_possible[i-1]
+            break
+        else
+            v = val[i]
+            k = k_possible[i]
+        end
+    end
+    return v, k
+end
+
+teste, test = lastpos(val, k_possible)
+
+@btime lastpos(val, k_possible)
+@btime maximum(val)
+
+
+value_function_brute = function(;v_0 = v0, z = grid_z, p_z = prob_z, k = grid_k, tol = 1e-4,
+    beta = 0.987, mu = 2, alpha = 1 / 3, delta = 0.012, rho = 0.95, sigma = 0.007, inert = 0)
+    
+    k_len = length(grid_k)
+    z_len = length(grid_z)
+
+    # Value function, policy function on c, policy function on k and variable for iteration
+    c, k_line, v_next = zeros(k_len, z_len), zeros(k_len, z_len), zeros(k_len, z_len)
+    value = v_0
+    count = 0
+    error = 1
+    while error > tol
+
+        Threads.@threads for state in 1:z_len
+            for capital in 1:k_len  
+                k_possible = k[z[state]*(k[capital]^alpha) + (1 - delta)*k[capital] .- k .> 0]    
+                v_possible = value[z[state]*(k[capital]^alpha) + (1 - delta)*k[capital] .- k .> 0, :]               
+                val = utility.(z[state]*(k[capital]^alpha) .- k_possible .+ (1 - delta)*k[capital]) .+ beta*v_possible*p_z[state, :]
+                v_next[capital, state] = maximum(val)
+                k_line[capital, state] = k_possible[argmax(val)]
+                c[capital, state] = z[state]*(k[capital]^alpha) - k_line[capital, state] + (1 - delta)* k[capital]
+            end # for k
+        end # for z
+        error = maximum(abs.(value - v_next))
+        value = copy((1 - inert)*v_next + inert*value)
+
+        count = count + 1
+    end # while
+
+    return value, k_line, c
+end; # function
